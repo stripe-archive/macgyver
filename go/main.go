f56e78501893101e708d4c97bfb0ccb54cf38dd1
Evan Broder <evan@stripe.com>
2015-08-13 14:03:20 -0700

    Use externally_connectable to limit access to the plugin

--- go/main.go
+++ go/main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gopherjs/gopherjs/js"
 	"golang.org/x/crypto/ssh/agent"
 )
-
-var AllowedClients = map[string]bool{
-	// Secure Shell
-	"pnhechapfaindjhompbnflcldabbghjo": true,
-	// Secure Shell (dev)
-	"okddffdblfhhnmhodogpojmfkjmhinfp": true,
-}
 
 func main() {
 	mga := NewMacGyverAgent()
@@ -23,13 +14,6 @@ func main() {
 		Get("onConnectExternal").
 		Call("addListener", func(port *js.Object) {
 		go func() {
-			sender := port.Get("sender").Get("id").String()
-			if !AllowedClients[sender] {
-				log.Printf("Received a connection from an unknown extension: %v", sender)
-				port.Call("disconnect")
-				return
-			}
-
 			p := NewAgentPort(port)
 			agent.ServeAgent(mga, p)
 		}()
